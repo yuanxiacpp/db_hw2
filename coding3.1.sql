865b493ed0b1d2bbb3c6a24c4d1f4b9d80ff07e0
Yuan Xia <yuan.xia@yale.edu>
2014-02-17 22:43:46 -0500

    finish debugging coding1

--- coding3.1.sql
+++ coding3.1.sql
@@ -12,52 +12,53 @@ from works
 where department_name = 'Income Tax Restructuring';
 
 --2. Name, salary and address (street, city) of META’s CFO. The IRS is extremely interested in the results of this query. (1)
-select employee_name, salary, employee.street+', '+employee.city as address 
-from works, employee 
-where works.employee_name = employee.employee_name and works.job_title = 'CFO'
+select E.employee_name, salary, E.street || ', ' || E.city as address 
+from works as W
+inner join employee as E on E.employee_name = W.employee_name 
+where W.job_title = 'CFO';
 
 --3. Names of employees who earn more than the average salary. (1)
 select employee_name 
-from works w1, (select avg(salary) as avg_salary from works)w2
-where w1.salary > w2.avg_salary 
+from works as W1, (select avg(salary) as avg_salary from works) as W2
+where W1.salary > W2.avg_salary;
 
 --4. Names of employees who live in cities other than their department’s headquarter city. (2)
-select employee_name
-from employee, department
-where employee.city != department.city
+select E.employee_name
+from employee as E
+inner join works as W on E.employee_name = W.employee_name
+inner join department as D on W.department_name = D.department_name
+where E.city != D.city;
 
 
 --5. Names of employees who live in the same cities and on the same streets as their managers. (3)
-select e1.employee_name
-from employee e1, employee e2, manages
-where e1.employee_name = manages.employee_name and
-      e2.employee_name = manages.manager_name and
-      e1.street = e2.street and
-      e1.city = e2.city
+select E1.employee_name
+from employee as E1, employee as E2, manages as M
+where E1.employee_name = M.employee_name and
+      E2.employee_name = M.manager_name and
+      E1.street = E2.street and
+      E1.city = E2.city;
 
 --6. Names of employees who earn more than the average salary of their department. (2)
 select employee_name
-from works w1, (select avg(salary) as avg_salary, department_name from works group by department_name)w2
-where w1.department_name = w2.department_name and
-      w1.salary > w2.avg_salary
+from works as W1, (select avg(salary) as avg_salary, department_name from works group by department_name) as W2
+where W1.department_name = W2.department_name and
+      W1.salary > W2.avg_salary;
 
 --7. Department with largest payroll (largest sum of all employee salaries). (3)
-select department_name
-from works w1, 
-     (select sum(salary) as total_salary, department_name 
+select W.department_name
+from 
+     (select sum(salary) as total_salary, department_name
       from works 
       group by department_name
       order by total_salary desc
-      limit 1)w2
-where w1.department_name = w2.department_name 
+     limit 1) as W;
 
 
 --8. Department with largest payroll per head. (2)
-select department_name
-from works w1,
-     (select sum(salary)/count(salary) as payroll_per_head, department_name
-      from works
+select W.department_name
+from 
+     (select avg(salary) as avg_salary, department_name
+      from works 
       group by department_name
-      order by payroll_per_head desc
-      limit 1)w2
-where w1.department_name = w2.department_name
+      order by avg_salary desc
+     limit 1) as W;
