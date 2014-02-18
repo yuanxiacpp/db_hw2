ea41e73c5b0d41f067c050f62eb2bd7287748253
Yuan Xia <yuan.xia@yale.edu>
2014-02-17 23:53:53 -0500

    finish debugging coding3

--- coding3.3.sql
+++ coding3.3.sql
@@ -5,17 +5,30 @@ loan(student_id, isbn, issue_date, due_date)
 */
 
 --1. For each author, names of students who borrowed more than three books by that author. (3)
-select count(t2.name) as readers_per_auther, t3.author
-from loan t1
-join student t2 on t1.student_id = t2.student_id
-join books t3 on t3.isbn = t1.isbn
-where readers_per_author > 3
-group by t3.author
+select T.author, T.name
+from (
+      select count(S.name) as readers_count, B.author, S.name
+      from loan as L
+      inner join student as S on S.student_id = L.student_id
+      inner join books as B on B.isbn = L.isbn
+      group by B.author, S.name
+     ) as T
+where T.readers_count > 3;
+
+
 --2. Names of students who borrowed all books written by ‘Avi Silberschatz’. (5)
+select S.name
+from student as S
+where not exists (
+	  	  (select isbn from books as B where author = 'Avi Silberschatz')
+		  except
+		  (select L.isbn from loan as L where L.student_id = S.student_id)
+      	  	 );
+
 
 --3. Names of students who borrowed books written by anyone with the last name “Sade” (assume that names are stored as ‘first middle last’). (2)
-select student.name
-from student, books, loan
-where student.student_id = loan.student_id and
-      loan.isbn = books.isbn and
-      books.author like '%Sade'
+select distinct S.name
+from student as S
+inner join loan as L on L.student_id = S.student_id
+inner join books as B on B.isbn = L.isbn
+where B.author like '%Sade';
